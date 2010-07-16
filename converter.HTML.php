6a3444e28f33698faa6af0ccd08eeb50880b3225
moechofe <service@moechofe.com>
2010-07-16 12:28:21 +0200

    fix: old class name

--- converter.HTML.php
+++ converter.HTML.php
@@ -9,7 +9,7 @@ abstract class ConverterHTML extends Converter
 {
   // {{{ convert_title()
 
-  public function convert_title( $title, DstyleDoc_Element $element )
+  public function convert_title( $title, Element $element )
   {
     return $title;
   }
@@ -25,7 +25,7 @@ abstract class ConverterHTML extends Converter
   // }}}
   // {{{ convert_licence()
 
-  public function convert_licence( $licence, DstyleDoc_Custom_Element $element )
+  public function convert_licence( $licence, CustomElement $element )
   {
     return '<tt>'.implode('<br />',$licence).'</tt>';
   }
@@ -33,7 +33,7 @@ abstract class ConverterHTML extends Converter
   // }}}
 	// {{{ convert_id()
 
-  public function convert_id( $id, DstyleDoc_Element $element )
+  public function convert_id( $id, Element $element )
   {
     if( is_array($id) )
       $id = implode('_', $id);
@@ -44,7 +44,7 @@ abstract class ConverterHTML extends Converter
   // }}}
   // {{{ convert_link()
 
-  public function convert_link( $id, $name, DstyleDoc_Element $element )
+  public function convert_link( $id, $name, Element $element )
   {
     return <<<HTML
 <a href="{$id}">{$name}</a>
